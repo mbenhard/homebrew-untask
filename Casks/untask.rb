989062cd4df7328a4766b329deb37c2b25d215b3
mbenhard <86309274+mbenhard@users.noreply.github.com>
2026-02-24 15:47:35 +0100

    chore: bump untask to v0.1.13

--- Casks/untask.rb
+++ Casks/untask.rb
@@ -1,6 +1,6 @@
 cask "untask" do
-  version "0.1.12"
-  sha256 "31c1ed093f32134d45f376cb840270f24428705e0fb9197947669d8662f63a38"
+  version "0.1.13"
+  sha256 "14897ff9f028551ecb3709f230d5b6200991d3fd807df6920875526a6c863238"
 
   url "https://github.com/mbenhard/untask/releases/download/v#{version}/Untask-darwin-arm64-#{version}.zip"
   name "Untask"
