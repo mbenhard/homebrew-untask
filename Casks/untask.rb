855c46220384d89ba1e7527a434792537557a45a
mbenhard <86309274+mbenhard@users.noreply.github.com>
2026-02-27 12:55:15 +0100

    chore: update untask cask to v0.1.15

--- Casks/untask.rb
+++ Casks/untask.rb
@@ -1,6 +1,6 @@
 cask "untask" do
-  version "0.1.14"
-  sha256 "6f8c48bfe296a55b75abcec9f3e00404958ea5056b6041cd1a215a22a1e05bc5"
+  version "0.1.15"
+  sha256 "2d93cf56fd4a33969b7a95b27c2c221b4e33997ca93bea321ad3893331038af4"
 
   url "https://github.com/mbenhard/untask/releases/download/v#{version}/Untask-darwin-arm64-#{version}.zip"
   name "Untask"
